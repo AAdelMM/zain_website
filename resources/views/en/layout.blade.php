916e2df35d25379a9272b5cb70ed6e892ccb2399
AAdelMM <ahmed.adel426@gmail.com>
2024-10-09 06:21:05 +0300

    add favicon

--- resources/views/en/layout.blade.php
+++ resources/views/en/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'ZainZwy')</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('images/zain.png')}}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
