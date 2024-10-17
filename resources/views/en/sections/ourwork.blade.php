8fb4066e2f308ed22b481e193697bb24fdbdc707
AAdelMM <ahmed.adel426@gmail.com>
2024-10-17 12:01:55 +0300

    edit our work section

--- resources/views/en/sections/ourwork.blade.php
+++ resources/views/en/sections/ourwork.blade.php
@@ -52,11 +52,19 @@
             min-width: calc(50% - 20px);
         }
     }
+    @media (max-width: 800px) {
+        .ourworkframe{
+            display:none;
+        }
+    }
 
     @media (max-width: 768px) {
         .card {
             flex: 0 0 calc(100% - 20px);
             min-width: calc(100% - 20px);
+        }
+        .ourworkframe{
+            display:none;
         }
     }
 
@@ -72,7 +80,7 @@
 <section id="ourwork">
     <div class="title  text-center text-[#26A7C0] mt-[5rem]">our work</div>
     <div class="mt-5 relative">
-        <img class="w-[100%] absolute z-[-2] top-10" src="{{asset('images/ourworkbg.png')}}" alt="">
+        <img class="w-[100%] absolute z-[-2] top-10 ourworkframe" src="{{asset('images/ourworkbg.png')}}" alt="">
         <div class="text-center lg:text-[2.7rem] md:text-[1.8rem] text-[1.2rem] text-[#034A58] font-extrabold  "><span class="text-[#26A7C0]">Our Work </span>That Exceeded</div>
         <div class="text-center lg:text-[2.7rem] md:text-[1.8rem] text-[1.2rem] text-[#034A58] font-extrabold">Expectations And Drove Business Growth</div>
         <div class="carousel-container w-[80%] mx-auto">
